44cd40cf50cf1c21a57bb45ee60625d9fc6d9929
Esra Akbulat <a.esra.akbulat@gmail.com>
2022-11-08 21:01:18 +0300

    js part finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
 //getting datas from API
-let words = [];
+let spells = [];
+let hints = [];
 const gettingDatas = () =>
-  fetch('https://random-word-api.herokuapp.com/word?number=10').then(
+  fetch('https://hp-api.herokuapp.com/api/spells?number=10%27').then(
     (response) => response.json().then((data) => loadData(data))
   );
 
 const loadData = (arr) => {
   for (item of arr) {
-    words.push(item);
+    spells.push(item.name);
+    hints.push(item.description);
   }
+
   const alphabet = [
     'a',
     'b',
@@ -37,21 +40,69 @@ const loadData = (arr) => {
     'y',
     'z',
   ];
-  const ulDOM = document.querySelector('#alphabet');
-  const creatingAlphabet = () => {
-    for (item of alphabet) {
-      let liDOM = document.createElement('li');
-      liDOM.innerText = item.toUpperCase();
-      ulDOM.append(liDOM);
-    }
-  };
 
-  creatingAlphabet();
+  let creatingButton = document.querySelector('#alphabet');
+
+  for (let i = 0; i < alphabet.length; i++) {
+    const button = document.createElement('button');
+    button.innerText = alphabet[i];
+    button.classList.add('btn');
+    creatingButton.appendChild(button);
+  }
 
   const randomNumber = Math.floor(Math.random() * 9);
 
-  const chooseWord = () => words[randomNumber];
+  const choosenWord = () => spells[randomNumber];
 
-  console.log(chooseWord());
+  const choosenHint = () => hints[randomNumber];
+
+  let word = choosenWord().toLowerCase();
+  let hint = choosenHint();
+
+  const wordContainer = document.createElement('p');
+  for (let i = 0; i < word.length; i++) {
+    wordContainer.innerText += '_';
+    wordContainer.classList.add('letter');
+  }
+  document.body.appendChild(wordContainer);
+  let hintDOM = document.querySelector('.hint');
+  let hintButton = document.querySelector('#showHint');
+  hintButton.addEventListener('click', () => {
+    hintDOM.innerText = hint;
+    hintDOM.style.display = 'block ';
+  });
+  console.log(word);
+  let lives = 5;
+  let alphabetButtons = document.querySelectorAll('.btn');
+  alphabetButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === button.innerText) {
+          wordContainer.innerText =
+            wordContainer.innerText.slice(0, i) +
+            button.innerText +
+            wordContainer.innerText.slice(i + 1);
+        }
+      }
+
+      let j = wordContainer.innerText.indexOf(button.innerText);
+      if (j === -1) {
+        lives -= 1;
+        const img = document.querySelector('#img');
+        if (lives === 4) {
+          img.setAttribute('src', '../images/1.png');
+        } else if (lives === 3) {
+          img.setAttribute('src', '../images/2.png');
+        } else if (lives === 2) {
+          img.setAttribute('src', '../images/3.png');
+        } else if (lives === 1) {
+          img.setAttribute('src', '../images/4.png');
+        } else if (lives === 0) {
+          img.setAttribute('src', '../images/5.png');
+        }
+      }
+    });
+  });
 };
+
 gettingDatas();
